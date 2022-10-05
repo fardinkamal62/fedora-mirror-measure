86280e6119dc11d7207c147823f71e91838c2a11
Fardin Kamal <fardinkamal62@protonmail.ch>
2022-10-05 14:49:49 +0600

    commenting fix

--- system_info.py
+++ system_info.py
@@ -3,10 +3,11 @@ import subprocess
 
 
 def hostnamectl(x):
-    cmd = f"hostnamectl | grep '{x}'"   # command to execute
+    # command to execute
+    cmd = f"hostnamectl | grep '{x}'"
 
-    temp = subprocess.Popen(            # storing in stodout
-        [cmd], shell=True, universal_newlines=True, stdout=subprocess.PIPE)
+    # storing the stdout
+    temp = subprocess.Popen([cmd], shell=True, universal_newlines=True, stdout=subprocess.PIPE)
 
     # get the output as a string
     output = str(temp.communicate()[0]).strip()
@@ -17,14 +18,10 @@ def architecture():
     archi = hostnamectl('Architecture')
     result = re.sub("Architecture:", "", archi)
     result = re.sub("-", "_", result).split()[0]
-    return result       # result = x86_64
+    return result  # result = x86_64 // string
 
 
 def os():
     os = hostnamectl('Operating System')
     result = re.findall("\d+", os)[0]
-    return int(result)         # result = 36 // int
-
-# archi = hostnamectl('Architecture')
-# os = hostnamectl('Operating System')
-# print(archi, os)
+    return int(result)  # result = 36 // int
