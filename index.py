a16a6b9c5ff5faf402531e4a940412f5f33b63aa
Nathaniel Handan <87228776+Tinny-Robot@users.noreply.github.com>
2023-03-27 12:22:41 +0100

    Update index.py
    Added comments to explain what each section of the code does.
    
    Used descriptive variable names to make the code more readable.
    
    Changed the os variable to os_version to avoid shadowing the built-in os module.
    
    Changed the mirror function call to fetch_mirrors to match the function name in the scrape module.
    
    Followed PEP 8 style guide recommendations for naming conventions, whitespace, and line length.

--- index.py
+++ index.py
@@ -1,26 +1,34 @@
 import os
-
 import system_info
 import scrape
 import speedtest
 
+# Define name and version of the tool
 name = "Fedora MiMe"
 version = '2.1.0'
 
+# Clear the terminal screen and print the name and version of the tool
 os.system('clear')
 print(f"{name}")
 print(f"Version: {version}\n")
 
+# Get system information using the sys_info function from the system_info module
 info = system_info.sys_info()
 
-os = info['version']
+# Get the version and architecture of the Fedora operating system from the system information
+os_version = info['version']
 archi = info['architecture']
 
-print(f"Found Operating System: Fedora {os}")
+# Print the found operating system version and architecture
+print(f"Found Operating System: Fedora {os_version}")
 print(f"Found Architecture: {archi}\n")
+
+# Get a list of Fedora mirrors using the mirror function from the scrape module
 print("Getting mirror list....\n")
+mirrors = scrape.fetch_mirrors(f"https://admin.fedoraproject.org/mirrormanager/mirrors/Fedora/{os_version}/{archi}")
 
-mirrors = scrape.mirror(f"https://admin.fedoraproject.org/mirrormanager/mirrors/Fedora/{os}/{archi}")
+# Run the speed test using the speed_test function from the speedtest module
+speedtest.speed_test(mirrors, os_version, archi)
 
-speedtest.speed_test(mirrors, os, archi)
+# Print a message thanking the user for using the tool
 print(f"Thank you for using {name}")
